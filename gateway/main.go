00f012f6a93b5a42761f876d5c8dbbc6eaf87ea1
bigphuc <phuctd@uiza.io>
2019-10-25 09:06:43 +0700

    Update error format return

--- gateway/main.go
+++ gateway/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
@@ -16,6 +17,29 @@ import (
 var (
 	entityEndpoint = flag.String("entity_endpoint", "localhost:50051", "endpoint of EntityService")
 )
+
+type errorBody struct {
+	Message string `json:"message,omitempty"`
+	Details struct {
+		Error string `json:"-"`
+	} `json:"detail"`
+}
+
+func CustomHTTPError(ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, _ *http.Request, err error) {
+	const fallback = `{"error": "failed to marshal error message"}`
+	fmt.Println("Custom HTTP error ->")
+	fmt.Println(grpc.ErrorDesc(err), grpc.Code(err), err)
+
+	w.Header().Set("Content-type", marshaler.ContentType())
+	w.WriteHeader(runtime.HTTPStatusFromCode(grpc.Code(err)))
+	jErr := json.NewEncoder(w).Encode(errorBody{
+		Message: grpc.ErrorDesc(err),
+	})
+
+	if jErr != nil {
+		w.Write([]byte(fallback))
+	}
+}
 
 func RunEndPoint(address string, opts ...runtime.ServeMuxOption) error {
 	ctx := context.Background()
